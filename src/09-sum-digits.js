ab07cdcadf441e38fdb75b5450c1a638082e8731
nsaveenko <nn.saveenko@gmail.com>
2021-04-09 19:34:05 +0300

    feat: add solving of 09-sum-digits task

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,26 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  const digits = n.toString().split('').map(Number);
+
+  let sum = 0;
+
+  for (let i = 0; i < digits.length; i++) {
+    sum += digits[i];
+  }
+
+  const sumFigures = sum.toString().split('').map(Number);
+
+  if (sumFigures.length === 2) {
+    sum = 0;
+
+    for (let i = 0; i < sumFigures.length; i++) {
+      sum += sumFigures[i];
+    }
+  }
+
+  return sum;
 }
 
 module.exports = getSumOfDigits;
